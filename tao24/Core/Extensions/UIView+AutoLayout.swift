2c866bbc74f054f22fea4f6599ee7c26ea52555b
Aditya Karki <adityakarki728@gmail.com>
2026-04-10 18:41:08 -0500

    feat: defined helper extensions for Autolayout functions on a UIView

--- tao24/Core/Extensions/UIView+AutoLayout.swift
+++ tao24/Core/Extensions/UIView+AutoLayout.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 extension UIView {
-    func pinToEdges(of container: UIView) {
+    
+    // Simplifies the process of making a view fill its parent container.
+    func pinToEdges(padding: CGFloat = 0) {
+        guard let superview = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
-            topAnchor.constraint(equalTo: container.topAnchor),
-            leadingAnchor.constraint(equalTo: container.leadingAnchor),
-            trailingAnchor.constraint(equalTo: container.trailingAnchor),
-            bottomAnchor.constraint(equalTo: container.bottomAnchor)
+            topAnchor.constraint(equalTo: superview.topAnchor, constant: padding),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: padding),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -padding),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -padding)
         ])
+    }
+    
+    // Perfectly aligns the center point of this view with its parent.
+    func centerInSuperview() {
+        guard let superview = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            centerXAnchor.constraint(equalTo: superview.centerXAnchor),
+            centerYAnchor.constraint(equalTo: superview.centerYAnchor)
+        ])
+    }
+    
+    // Provides a quick way to set fixed width and height values.
+    func setDimensions(height: CGFloat? = nil, width: CGFloat? = nil) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
     }
 }
